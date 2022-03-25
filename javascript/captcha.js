b6201141e5775fbad53aecd8f7b8e030092eed10
Soham Sasan <soham.satsangi12@gmail.com>
2022-03-25 12:36:00 -0400

    captcha.js
    Added code so that button redirects to main.html, and changed some other things.

--- javascript/captcha.js
+++ javascript/captcha.js
@@ -1,3 +1,5 @@
+
+// document.querySelector() is used to select an element from the document using its ID
 let captchaText = document.querySelector('#captcha');
 var ctx = captchaText.getContext("2d");
 ctx.font = "30px Roboto";
@@ -10,16 +12,22 @@ let output = document.querySelector('#output');
 let refreshButton = document.querySelector('#refreshButton');
 
 
+// alphaNums contains the characters with which you want to create the CAPTCHA
 let alphaNums = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let emptyArr = [];
 
-
+// This loop generates a random string of 7 characters using alphaNums
+// Further this string is displayed as a CAPTCHA
 for (let i = 1; i <= 7; i++) {
     emptyArr.push(alphaNums[Math.floor(Math.random() * alphaNums.length)]);
 }
 var c = emptyArr.join('');
 ctx.fillText(emptyArr.join(''),captchaText.width/4, captchaText.height/2);
 
+
+// This event listener is stimulated whenever the user press the "Enter" button
+// "Correct!" or "Incorrect, please try again" message is
+// displayed after validating the input text with CAPTCHA
 userText.addEventListener('keyup', function(e) {
     if (e.keyCode === 13) {
         if (userText.value === c) {
@@ -35,10 +43,11 @@ userText.addEventListener('keyup', function(e) {
 submitButton.addEventListener('click', function() {
     if (userText.value === c) {
         output.classList.add("correctCaptcha");
-        output.innerHTML = "Yes! Click YES on Top-Right corner.";
+        output.innerHTML = "Correct!";
+        window.location.replace("../html/main.html");
     } else {
         output.classList.add("incorrectCaptcha");
-        output.innerHTML = "Incorrect, please try again by clicking Refresh.";
+        output.innerHTML = "Incorrect, please try again";
     }
 });
 
@@ -53,4 +62,3 @@ refreshButton.addEventListener('click', function() {
     ctx.fillText(refreshArr.join(''),captchaText.width/4, captchaText.height/2);
     output.innerHTML = "";
 });
-//Code Taken from GitHub.
