a0093a8a559d8fa5ff46bb5c54562e128b717c6e
Soham Sasan <soham.satsangi12@gmail.com>
2022-04-07 09:44:37 -0400

    moreJava.java
    More Java stuff.

---
+++ java/moreJava.java
@@ -0,0 +1,8 @@
+public class moreJava {
+    public static void main(String[] args){
+        var soham = 0;
+        if(soham == 0){
+            System.out.println("Hey there!");
+        }
+    }
+}
