debba038d23f3109f3d17ce832f8ae99c352f081
Soham Sasan <soham.satsangi12@gmail.com>
2022-11-04 13:25:50 -0400

    moreJava.java
    Deleted JAVA file.

--- java/moreJava.java
+++
@@ -1,8 +0,0 @@
-public class moreJava {
-    public static void main(String[] args){
-        var soham = 0;
-        if(soham == 0){
-            System.out.println("Hey there!");
-        }
-    }
-}
